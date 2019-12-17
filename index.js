ae7d94a177a7bb5ee9be2174ea85f61a435f3dbc
Matthew Robinson <matt_robinson1@yahoo.com>
2019-12-17 22:45:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,4 +4,11 @@ let formData = {
   dogBreed: "Poodle"
 }; 
 
-
+let configObj = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+  },
+  body: JSON.stringify(formData)
+};
