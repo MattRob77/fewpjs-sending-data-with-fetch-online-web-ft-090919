e486833b1de1d33ecda96c5ddfa473f80b18c677
Matthew Robinson <matt_robinson1@yahoo.com>
2019-12-17 22:48:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,45 +1,5 @@
 // Add your code here
-let formData = { 
-  dogName: "Byron", 
-  dogBreed: "Poodle"
-}; 
 
-let configObj = {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json",
-    "Accept": "application/json"
-  },
-  body: JSON.stringify(formData)
-};
-
-function addDog() {
-  fetch("http://localhost:3000/dogs", configObj)
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(object) {
-      console.log(object);
-    })
-    .catch(function(error) {
-      alert("Bad things! Ragnarők!");
-      console.log(error.message);
-    });
-}
-
-function configObj2(name, email) {
-  return {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Accept: "application/json"
-    },
-    body: JSON.stringify({
-      name: `${name}`,
-      email: `${email}`
-    })
-  };
-}
 
 function submitData(name, email) {
   return fetch("http://localhost:3000/users", {
