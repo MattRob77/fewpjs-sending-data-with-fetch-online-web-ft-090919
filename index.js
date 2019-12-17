dbbfce799d3edfddb064260a4150119a0879a609
Matthew Robinson <matt_robinson1@yahoo.com>
2019-12-17 22:48:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,3 +12,56 @@ let configObj = {
   },
   body: JSON.stringify(formData)
 };
+
+function addDog() {
+  fetch("http://localhost:3000/dogs", configObj)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(object) {
+      console.log(object);
+    })
+    .catch(function(error) {
+      alert("Bad things! Ragnarők!");
+      console.log(error.message);
+    });
+}
+
+function configObj2(name, email) {
+  return {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    },
+    body: JSON.stringify({
+      name: `${name}`,
+      email: `${email}`
+    })
+  };
+}
+
+function submitData(name, email) {
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    },
+    body: JSON.stringify({
+      name,
+      email
+    })
+  })
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(object) {
+      document.body.innerHTML = object["id"];
+    })
+    .catch(function(error) {
+      debugger;
+      alert("Bad things! Ragnarők!");
+      document.body.innerHTML = error.message;
+    });
+}
